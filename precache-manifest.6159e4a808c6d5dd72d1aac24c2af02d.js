c8b0fed7d69360e5f4bb7c9f9e13fb79f0e8c33a
Rodney Oliveira <rodney@ictweb.com.br>
2020-05-15 14:20:54 -0300

    initial

---
+++ precache-manifest.6159e4a808c6d5dd72d1aac24c2af02d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c653c822613e63a84b0bf1a5da1c04b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e470654ad5dc2cde25ad",
+    "url": "/static/css/main.b586361f.chunk.css"
+  },
+  {
+    "revision": "54698cc9f0a0c546db03",
+    "url": "/static/js/2.875d88ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.875d88ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e470654ad5dc2cde25ad",
+    "url": "/static/js/main.a5caa424.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "453e928cc8bbf280bda64e04c4300e66",
+    "url": "/static/media/bg-form.453e928c.png"
+  },
+  {
+    "revision": "b87fd70158ce115a0b09d1353ad4e067",
+    "url": "/static/media/oggi.b87fd701.png"
+  }
+]);
